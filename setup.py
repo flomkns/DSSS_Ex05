1122d0acac7a9375813a2ff9bf539a2c8e1786ba
flomkns <flo.mehrkens@gmail.com>
2022-11-28 16:17:04 +0100

    ADDED setup.py generated with pycharm.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='snowflake',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/flomkns/DSSS_Ex05',
+    license='Apache-2.0 license',
+    author='Florian Mehrkens',
+    author_email='flo.mehrkens@fau.de',
+    description='let it snow (Ex05)'
+)
