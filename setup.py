9aaca46e5c3a0cf1bd2bea8c66baed127a9e346b
flomkns <flo.mehrkens@gmail.com>
2022-11-29 14:31:40 +0100

    CHANGED PACKAGES in setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import find_packages
 setup(
     name='snowflake',
     version='1.0.2',
-    packages=['let_it_snow'],
+    packages=find_packages(where="snowflake"),
+    package_dir={"": "snowflake"},
     url='https://github.com/flomkns/DSSS_Ex05',
     license='Apache-2.0 license',
     author='Florian Mehrkens',
