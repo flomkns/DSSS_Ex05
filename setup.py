ea9d2fe04fa9318aedee5452380ad02c95c2d4dc
flomkns <flo.mehrkens@gmail.com>
2022-11-29 13:27:41 +0100

    CHANGED packages to auto find_packages() in setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+import setuptools
 
 setup(
     name='snowflake',
     version='1.0.0',
-    packages=['snowflake'],
+    packages=setuptools.find_packages(),
     url='https://github.com/flomkns/DSSS_Ex05',
     license='Apache-2.0 license',
     author='Florian Mehrkens',
